8dd2ecbd171e3097aa2d9e1e0ddcd5eb44e88cd7
TanZhiJie <TanZhiJie@windows10.microdone.cn>
2019-01-15 09:37:36 +0800

    添加项目

---
+++ lusy/src/main/java/com/lusy/lusy/LusyApplication.java
@@ -0,0 +1,14 @@
+package com.lusy.lusy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LusyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LusyApplication.class, args);
+	}
+
+}
+
